92b89fb67d55f3ef94c655d47f9ef71e1426a6f6
PRANJAL <pranjal.mishra@housejoy.in>
2021-04-24 21:25:17 +0530

    lecture 4.2

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv')
 const products = require('./data/products')
+const connectDB = require('./config/db')
 
 dotenv.config()
+
+connectDB()
+
 const app = express()
 
 
