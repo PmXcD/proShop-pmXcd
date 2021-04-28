48ee8f48b42ddfbff8aad1391e0adb3ef518a6a0
PRANJAL <pranjal.mishra@housejoy.in>
2021-04-28 15:19:42 +0530

    lecture 6.5

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
-import { addToCart } from '../actions/cartActions'
+import { addToCart, removeFromCart } from '../actions/cartActions'
 import { Link } from 'react-router-dom'
 
 const CartScreen = ({ match, location, history }) => {
@@ -21,7 +21,7 @@ const CartScreen = ({ match, location, history }) => {
     }, [dispatch, productId, qty])
 
     const removeFromCartHandler = (id) => {
-        console.log('removed', id)
+        dispatch(removeFromCart(id))
     }
 
     const checkoutHandler = () => {
