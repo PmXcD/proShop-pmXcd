654f91a7ef8b80569dcee588556d2ec99b9e21ea
PRANJAL <pranjal.mishra@housejoy.in>
2021-04-29 08:46:59 +0530

    lecture 7.4

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler';
 import User from '../modals/userModal.js';
+import generateToken from '../utils/generateToken';
 
 //@desc    Auth user & get token
 //@route   POST /api/users/login
@@ -14,7 +15,7 @@ const authUser = asyncHandler(async(req, res) => {
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: null
+            token: generateToken(user._id)
         })
     } else {
         res.status(401)
