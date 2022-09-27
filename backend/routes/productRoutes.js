9b8544f5228f491578c4581895804a31b36bec9d
Pranjal Mishra <maniprabhanshu01@gmail.com>
2022-09-27 19:06:24 +0530

    added API for adding reviews by user

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getProducts, getProductById, deleteProduct, createSampleProduct, updateProduct} from '../controllers/productController.js'
+import { getProducts, getProductById, deleteProduct, createSampleProduct, updateProduct, createProductReview} from '../controllers/productController.js'
 import { protect, isAdmin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.route('/').get(getProducts).post(protect, isAdmin, createSampleProduct)
 router.route('/:id').get(getProductById).delete(protect, isAdmin, deleteProduct).put(protect, isAdmin, updateProduct)
+router.route('/:id/reviews').post(protect, createProductReview)
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
 import colors from 'colors'
+import morgan from 'morgan'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
@@ -16,6 +17,10 @@ connectDB()
 const app = express()
 
 app.use(express.json())
+
+if(process.env.NODE_ENV === "development"){
+    app.use(morgan('dev'))
+}
 
 
 app.get('/', (req, res) => {
